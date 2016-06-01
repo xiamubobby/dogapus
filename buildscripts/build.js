4c8d8d365be922d32b517e15e189fa9d4cb90bc0
夏木 <natsuki@BobbyShit.local>
2016-06-01 11:31:32 +0800

    automaton done

--- buildscripts/build.js
+++ buildscripts/build.js
@@ -3,44 +3,47 @@
  */
 "use strict";
 const shell = require("shelljs");
-const admzip = require("adm-zip");
+const decompresszip = require("decompress-zip");
 const fs = require("fs");
-const target = "osx";
+const target = "win32";
 const electronPrebuiltDir = fs.readdirSync(`${__dirname}/../electron_prebuilt/${target}/`);
 let zip;
 for (const path of electronPrebuiltDir) {
     if (path.endsWith(".zip")) {
-        zip = new admzip(`${__dirname}/../electron_prebuilt/${target}/${path}`);
+        zip = new decompresszip(`${__dirname}/../electron_prebuilt/${target}/${path}`);
         break;
     }
 }
 const outDir = `${__dirname}/../out/${target}/`;
 shell.mkdir('-p', outDir);
-zip.extractAllTo(outDir, true);
-// let appDir;
-// switch (target) {
-//     case "osx":
-//         appDir = `${__dirname}/../out/osx/Electron.app/Contents/Resources/app/`
-// }
-// shell.mkdir('-p', appDir);
-//
-// const toBeCopieds = fs.readdirSync("./");
-// for (const outer of [".git", "buildscripts", "electron_prebuilt", "out", "typings", ".gitignore", "tsconfig.json", "typings.json", ".idea"]) {
-//     if (toBeCopieds.indexOf(outer) > -1) {
-//         toBeCopieds.splice(toBeCopieds.indexOf(outer), 1);
-//     }
-// }
-// for (const name of toBeCopieds.filter(function (value: string, index, array) {
-//     return !((value.endsWith(".ts")) || (value.endsWith(".js.map")))
-// })) {
-//     shell.cp('-r', `${__dirname}/../${name}`, appDir);    
-// }
-//
-// const flPlugins = fs.readdirSync(`${appDir}/PepperFlash`);
-// flPlugins.filter(function (value, index, array) {
-//    return !(value == target);
-// }).forEach(function (value, index) {
-//     shell.rm("-rf", `${appDir}/PepperFlash/${value}`)
-// });
-//
+zip.extract({
+    path: outDir
+});
+let appDir;
+switch (target) {
+    case "osx":
+        appDir = `${__dirname}/../out/osx/Electron.app/Contents/Resources/app/`;
+        break;
+    case "win32":
+        appDir = `${__dirname}/../out/win32/resources/app/`;
+        break;
+}
+shell.mkdir('-p', appDir);
+const toBeCopieds = fs.readdirSync("./");
+for (const outer of [".git", "buildscripts", "electron_prebuilt", "out", "typings", ".gitignore", "tsconfig.json", "typings.json", ".idea"]) {
+    if (toBeCopieds.indexOf(outer) > -1) {
+        toBeCopieds.splice(toBeCopieds.indexOf(outer), 1);
+    }
+}
+for (const name of toBeCopieds.filter(function (value, index, array) {
+    return !((value.endsWith(".ts")) || (value.endsWith(".js.map")));
+})) {
+    shell.cp('-r', `${__dirname}/../${name}`, appDir);
+}
+const flPlugins = fs.readdirSync(`${appDir}/PepperFlash`);
+flPlugins.filter(function (value, index, array) {
+    return !(value == target);
+}).forEach(function (value, index) {
+    shell.rm("-rf", `${appDir}/PepperFlash/${value}`);
+});
 //# sourceMappingURL=build.js.map
--- buildscripts/build.ts
+++ buildscripts/build.ts
@@ -3,15 +3,15 @@
  */
 
 import shell = require("shelljs")
-import admzip = require("adm-zip")
+import decompresszip = require("decompress-zip")
 import fs = require("fs")
 
-const target = "osx";
+const target = "win32";
 const electronPrebuiltDir = fs.readdirSync(`${__dirname}/../electron_prebuilt/${target}/`);
 let zip;
 for (const path of electronPrebuiltDir) {
     if (path.endsWith(".zip")) {
-        zip = new admzip(`${__dirname}/../electron_prebuilt/${target}/${path}`);
+        zip = new decompresszip(`${__dirname}/../electron_prebuilt/${target}/${path}`);
         break;
     }
 }
@@ -20,31 +20,37 @@ const outDir = `${__dirname}/../out/${target}/`;
 
 shell.mkdir('-p', outDir);
 
-zip.extractAllTo(outDir, true);
+zip.extract({
+    path: outDir
+});
 
-// let appDir;
-// switch (target) {
-//     case "osx":
-//         appDir = `${__dirname}/../out/osx/Electron.app/Contents/Resources/app/`
-// }
-// shell.mkdir('-p', appDir);
-//
-// const toBeCopieds = fs.readdirSync("./");
-// for (const outer of [".git", "buildscripts", "electron_prebuilt", "out", "typings", ".gitignore", "tsconfig.json", "typings.json", ".idea"]) {
-//     if (toBeCopieds.indexOf(outer) > -1) {
-//         toBeCopieds.splice(toBeCopieds.indexOf(outer), 1);
-//     }
-// }
-// for (const name of toBeCopieds.filter(function (value: string, index, array) {
-//     return !((value.endsWith(".ts")) || (value.endsWith(".js.map")))
-// })) {
-//     shell.cp('-r', `${__dirname}/../${name}`, appDir);    
-// }
-//
-// const flPlugins = fs.readdirSync(`${appDir}/PepperFlash`);
-// flPlugins.filter(function (value, index, array) {
-//    return !(value == target);
-// }).forEach(function (value, index) {
-//     shell.rm("-rf", `${appDir}/PepperFlash/${value}`)
-// });
-//
+let appDir;
+switch (target) {
+    case "osx":
+        appDir = `${__dirname}/../out/osx/Electron.app/Contents/Resources/app/`;
+        break;
+    case "win32":
+        appDir = `${__dirname}/../out/win32/resources/app/`;
+        break;
+}
+shell.mkdir('-p', appDir);
+
+const toBeCopieds = fs.readdirSync("./");
+for (const outer of [".git", "buildscripts", "electron_prebuilt", "out", "typings", ".gitignore", "tsconfig.json", "typings.json", ".idea"]) {
+    if (toBeCopieds.indexOf(outer) > -1) {
+        toBeCopieds.splice(toBeCopieds.indexOf(outer), 1);
+    }
+}
+for (const name of toBeCopieds.filter(function (value: string, index, array) {
+    return !((value.endsWith(".ts")) || (value.endsWith(".js.map")))
+})) {
+    shell.cp('-r', `${__dirname}/../${name}`, appDir);
+}
+
+const flPlugins = fs.readdirSync(`${appDir}/PepperFlash`);
+flPlugins.filter(function (value, index, array) {
+   return !(value == target);
+}).forEach(function (value, index) {
+    shell.rm("-rf", `${appDir}/PepperFlash/${value}`)
+});
+
