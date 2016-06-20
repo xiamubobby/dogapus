ba8e60cb8551397b38759441054613127ec9896d
夏木 <natsuki@BobbyShit.local>
2016-06-20 18:08:19 +0800

    .gitignore corrections

--- buildscripts/clean.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * Created by natsuki on 16/6/6.
- */
-const shell = require("shelljs");
-shell.rm("-rf", `${__dirname}/../out`);
-//# sourceMappingURL=clean.js.map
--- indexinjections.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by natsuki on 16/6/20.
- */
-window.loki = require("./lokis/loki_manager");
-//# sourceMappingURL=indexinjections.js.map
